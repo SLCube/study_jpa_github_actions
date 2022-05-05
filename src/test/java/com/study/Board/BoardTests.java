4b097f9ab9247d08e4f75105895ecee1c23442b2
kangkyoungmo <kgm0096@naver.com>
2022-05-05 18:22:21 +0900

    DB환경설정 및 게시글 CRD 테스트

---
+++ src/test/java/com/study/Board/BoardTests.java
@@ -0,0 +1,53 @@
+package com.study.Board;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.study.board.entity.Board;
+import com.study.board.entity.BoardRepository;
+
+@SpringBootTest
+public class BoardTests {
+
+	@Autowired
+	BoardRepository boardRepository;
+	
+	@Test
+	void save() {
+		Board params = Board.builder()
+				.title("1번 게시글 제목")
+				.content("1번 게시글 내용")
+				.writer("도뎡이")
+				.deleteYn('N')
+				.build();
+		
+		boardRepository.save(params);
+		
+		Board entity = boardRepository.findById((long) 1).get();
+		assertThat(entity.getTitle()).isEqualTo("1번 게시글 제목");
+		assertThat(entity.getContent()).isEqualTo("1번 게시글 내용");
+		assertThat(entity.getWriter()).isEqualTo("도뎡이");
+	}
+	
+	@Test
+	void findAll() {
+		
+		long boardsCount = boardRepository.count();
+		
+		List<Board> boards = boardRepository.findAll();
+	}
+	
+	@Test
+	void delete() {
+		
+		Board entity = boardRepository.findById((long) 1).get();
+		
+		boardRepository.delete(entity);
+	}
+	
+}
