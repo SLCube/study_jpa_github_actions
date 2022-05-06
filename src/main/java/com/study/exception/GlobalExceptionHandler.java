2450da3aee15835f4dc513ae12738993948a62a4
kangkyoungmo <kgm0096@naver.com>
2022-05-06 16:09:21 +0900

    [CHORE] global exception handling 추가

---
+++ src/main/java/com/study/exception/GlobalExceptionHandler.java
@@ -0,0 +1,48 @@
+package com.study.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+	/**
+	 * Developer Custom Exception
+	 * @param e
+	 * @return
+	 */
+	@ExceptionHandler(CustomException.class)
+	protected ResponseEntity<ErrorResponse> handleCustomException(final CustomException e){
+		log.error("handleCustomException : {}", e.getErrorCode());
+		return ResponseEntity
+				.status(e.getErrorCode().getStatus().value())
+				.body(new ErrorResponse(e.getErrorCode()));
+	}
+	
+	/**
+	 * HTTP 405 Exception
+	 * @param e
+	 * @return
+	 */
+	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+	protected ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(final HttpRequestMethodNotSupportedException e) {
+		log.error("handleHttpRequestMethodNotSupportedException : {}", e.getMessage());
+		return ResponseEntity
+				.status(ErrorCode.METHOD_NOT_ALLOWED.getStatus().value())
+				.body(new ErrorResponse(ErrorCode.METHOD_NOT_ALLOWED));
+	}
+	
+	@ExceptionHandler(Exception.class)
+	protected ResponseEntity<ErrorResponse> handleException(final Exception e){
+		log.error("handleException : {}", e.getMessage());
+		return ResponseEntity
+				.status(ErrorCode.INTERNET_SERVER_ERROR.getStatus().value())
+				.body(new ErrorResponse(ErrorCode.INTERNET_SERVER_ERROR));
+	}
+	
+}
