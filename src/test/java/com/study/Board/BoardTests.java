233efbafbcf1620059e95a01bd4afe3d53f548c0
kangkyoungmo <kgm0096@naver.com>
2022-05-09 15:42:17 +0900

    modified boardTest.java

--- src/test/java/com/study/Board/BoardTests.java
+++ src/test/java/com/study/Board/BoardTests.java
@@ -17,30 +17,30 @@ public class BoardTests {
 	@Autowired
 	BoardRepository boardRepository;
 	
-	@Test
-	void save() {
-		Board params = Board.builder()
-				.title("1번 게시글 제목")
-				.content("1번 게시글 내용")
-				.writer("도뎡이")
-				.deleteYn('N')
-				.build();
-		
-		boardRepository.save(params);
-		
+//	@Test
+//	void save() {
+//		Board params = Board.builder()
+//				.title("1번 게시글 제목")
+//				.content("1번 게시글 내용")
+//				.writer("도뎡이")
+//				.deleteYn('N')
+//				.build();
+//		
+//		boardRepository.save(params);
+//		
 //		Board entity = boardRepository.findById((long) 1).get();
 //		assertThat(entity.getTitle()).isEqualTo("1번 게시글 제목");
 //		assertThat(entity.getContent()).isEqualTo("1번 게시글 내용");
 //		assertThat(entity.getWriter()).isEqualTo("도뎡이");
-	}
+//	}
 	
-	@Test
-	void findAll() {
-		
-		long boardsCount = boardRepository.count();
-		
-		List<Board> boards = boardRepository.findAll();
-	}
+//	@Test
+//	void findAll() {
+//		
+//		long boardsCount = boardRepository.count();
+//		
+//		List<Board> boards = boardRepository.findAll();
+//	}
 	
 //	@Test
 //	void delete() {
