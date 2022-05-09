c69f39c601207e7aaed8b98a7af3ee4ef492fa3c
kangkyoungmo <kgm0096@naver.com>
2022-05-09 10:21:52 +0900

    modified test code

--- src/test/java/com/study/Board/BoardTests.java
+++ src/test/java/com/study/Board/BoardTests.java
@@ -17,37 +17,37 @@ public class BoardTests {
 	@Autowired
 	BoardRepository boardRepository;
 	
-//	@Test
-//	void save() {
-//		Board params = Board.builder()
-//				.title("1번 게시글 제목")
-//				.content("1번 게시글 내용")
-//				.writer("도뎡이")
-//				.deleteYn('N')
-//				.build();
-//		
-//		boardRepository.save(params);
-//		
-//		Board entity = boardRepository.findById((long) 1).get();
-//		assertThat(entity.getTitle()).isEqualTo("1번 게시글 제목");
-//		assertThat(entity.getContent()).isEqualTo("1번 게시글 내용");
-//		assertThat(entity.getWriter()).isEqualTo("도뎡이");
-//	}
-//	
-//	@Test
-//	void findAll() {
-//		
-//		long boardsCount = boardRepository.count();
-//		
-//		List<Board> boards = boardRepository.findAll();
-//	}
-//	
-//	@Test
-//	void delete() {
-//		
-//		Board entity = boardRepository.findById((long) 3).get();
-//		
-//		boardRepository.delete(entity);
-//	}
+	@Test
+	void save() {
+		Board params = Board.builder()
+				.title("1번 게시글 제목")
+				.content("1번 게시글 내용")
+				.writer("도뎡이")
+				.deleteYn('N')
+				.build();
+		
+		boardRepository.save(params);
+		
+		Board entity = boardRepository.findById((long) 1).get();
+		assertThat(entity.getTitle()).isEqualTo("1번 게시글 제목");
+		assertThat(entity.getContent()).isEqualTo("1번 게시글 내용");
+		assertThat(entity.getWriter()).isEqualTo("도뎡이");
+	}
+	
+	@Test
+	void findAll() {
+		
+		long boardsCount = boardRepository.count();
+		
+		List<Board> boards = boardRepository.findAll();
+	}
+	
+	@Test
+	void delete() {
+		
+		Board entity = boardRepository.findById((long) 3).get();
+		
+		boardRepository.delete(entity);
+	}
 	
 }
